886bef49fa8bb058c34e1eb37a8148617a8d0049
Dalik17 <dalik17@hotmail.com>
2011-07-14 00:50:06 -0700

    Edited plugins/join-leave.py via GitHub

--- plugins/join-leave.py
+++ plugins/join-leave.py
@@ -4,23 +4,23 @@ from util import hook
 
 @hook.command
 def join(inp, input=None):
-	"EvilDalik: join <channel>"
+	"MCBot: join <channel>"
 	if input.nick not in input.bot.config["admins"]:
 		return "Only bot admins can use this command!"
 	chan = inp.split(' ', 1)
 	#if len(chan) != 1:
-		#return "Usage: TerraBot: join <channel>"
+		#return "Usage: MCBot: join <channel>"
 	input.say("Joining " + inp + " :D")
 	input.conn.send("JOIN " + inp)
 
 @hook.command
 def leave(inp, input=None):
-	"EvilDalik: leave <channel>"
+	"MCBot: leave <channel>"
 	if input.nick not in input.bot.config["admins"]:
 		return "Only bot admins can use this command!"
 	chan = inp.split(' ', 1)
 	#if len(chan) != 1:
-		#return "Usage: TerraBot: leave <channel>"
+		#return "Usage: MCBot: leave <channel>"
 	input.say("Kicked out of " + inp + " >:(")
 	input.conn.send("PART " + inp)
 
